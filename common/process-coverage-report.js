d0b4841e1ee4474163132711fc74bd781da7eded
Carlos Solar <carlosz@lagash.com>
2019-09-29 22:54:27 -0300

    gitignore

---
+++ common/process-coverage-report.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-shadow */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable promise/always-return */
+/* eslint-disable promise/no-promise-in-callback */
+/* eslint-disable handle-callback-err */
+/* eslint-disable no-unused-vars */
+
+const fs = require('fs');
+const path = require('path');
+const inline = require('inline-css');
+
+const TEST_RESULTS_DIRECTORY = './.test_output';
+const CODE_COVERAGE_DIRECTORY = './.test_output/coverage';
+
+console.log('process-coverage-report inicializando');
+
+fs.readdir(CODE_COVERAGE_DIRECTORY, (err, files) => {
+  if (err) {
+    console.log(`process-coverage-report inline.then finalizado err: [${err}]`);
+    throw new Error(err);
+  }
+
+  const reports = files.filter((report) => report.endsWith('.html'));
+
+  const baseCss = fs.readFileSync(
+    path.resolve(path.join(CODE_COVERAGE_DIRECTORY, 'base.css')),
+    'utf8'
+  );
+
+  const prettifyCss = fs.readFileSync(
+    path.resolve(path.join(CODE_COVERAGE_DIRECTORY, 'prettify.css')),
+    'utf8'
+  );
+
+  const prettifyJs = fs.readFileSync(
+    path.resolve(path.join(CODE_COVERAGE_DIRECTORY, 'prettify.js')),
+    'utf8'
+  );
+
+  reports.forEach((report) => {
+    const filePath = path.join(CODE_COVERAGE_DIRECTORY, report);
+    const options = {
+      url: `file://${path.resolve(filePath)}`,
+      extraCss: `body { background-color: #fff !important; } .pad1 { padding: 0; } div.wrapper { text-decoration: underline; }  ${baseCss} ${prettifyCss}`,
+    };
+
+    console.log(`process-coverage-report inline.then: ${filePath}`);
+
+    fs.readFile(path.resolve(filePath), (err, data) => {
+      inline(data, options)
+        .then((html) => {
+          console.log('process-coverage-report inline.then');
+          const outputFile = path.join(TEST_RESULTS_DIRECTORY, report);
+          fs.writeFile(outputFile, html, (err) => {
+            if (err) {
+              console.log(`process-coverage-report inline.then finalizado err: [${err}]`);
+              throw err;
+            }
+            console.log('process-coverage-report inline.then finalizado');
+            return undefined;
+          });
+        })
+        .catch((ex) => {
+          console.log(`process-coverage-report finalizado inline.catch ex: [${ex}]`);
+          console.log(ex);
+          return undefined;
+        });
+    });
+  });
+});
---
+++ src/test/handlers/products.spec.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+const supertest = require('supertest');
+
+// eslint-disable-next-line no-unused-vars
+let server;
+beforeEach(() => {
+  // eslint-disable-next-line global-require
+  server = require('../../../server');
+});
+after(() => {});
+
+process.env.TEST_API = 'http://localhost:3101';
+process.env.NODE_ENV = 'integration-test';
+
+const apiClient = supertest.agent(process.env.TEST_API);
+
+describe('PRODUCT API', () => {
+  describe('Should return data by GET', () => {
+    it('OK /v1/products', (done) => {
+      apiClient
+        .get('/v1/products')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            console.clear();
+            console.log('NOK /v1/products');
+            console.log(err);
+            done(err);
+          } else {
+            done();
+          }
+        });
+    });
+  });
+});
